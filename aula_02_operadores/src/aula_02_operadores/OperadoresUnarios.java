b81ea6433ec7fada5b8c60be4afc3ba7632ea97b
Rafael Queiroz <rafaelproinfo@gmail.com>
2026-06-12 10:46:05 -0300

    Aula 02 - Exemplos sobre Operadores

---
+++ aula_02_operadores/src/aula_02_operadores/OperadoresUnarios.java
@@ -0,0 +1,18 @@
+package aula_02_operadores;
+
+public class OperadoresUnarios {
+	
+	public static void main(String[] args) {
+		
+		int contador = 1000;
+		
+		System.out.println("Valor inicial de contador: " + contador);
+		
+		//System.out.println("Valor Pré Incrementado: " + (++ contador));
+		
+		System.out.println("Valor Pós Incrementado: " + (contador ++));
+		
+		System.out.println("Novo valor de contador: " + contador);
+		
+	}
+}
