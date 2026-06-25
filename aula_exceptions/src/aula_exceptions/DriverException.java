ed923fcebb9836c46b07810887520b4e25b07aaa
Rafael Queiroz <rafaelproinfo@gmail.com>
2026-06-24 23:54:23 -0300

    Aula Exceptions

---
+++ aula_exceptions/src/aula_exceptions/DriverException.java
@@ -0,0 +1,17 @@
+package aula_exceptions;
+
+public class DriverException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DriverException() {	}
+
+	public DriverException(String mensagem) {	
+		super(mensagem);
+	}
+	
+	
+}
